f77772e69fd534548ca0c1bb4935306895947d59
piotrdeptala <deptala.piotr@gmail.com>
2023-03-04 15:34:26 +0100

    add pdf generator and test controller

--- src/main/java/pl/deptala/piotr/queencelldatecalculator/web/CalenderController.java
+++ src/main/java/pl/deptala/piotr/queencelldatecalculator/web/CalenderController.java
@@ -1,15 +1,20 @@
 package pl.deptala.piotr.queencelldatecalculator.web;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.deptala.piotr.queencelldatecalculator.service.CalenderService;
+import pl.deptala.piotr.queencelldatecalculator.service.PdfGeneratorService;
 import pl.deptala.piotr.queencelldatecalculator.web.model.QueenBeesReproductionCalendarModel;
 
-import java.time.LocalDate;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.logging.Logger;
 
 @Controller
@@ -19,9 +24,11 @@ public class CalenderController {
     private static final Logger LOGGER = Logger.getLogger(CalenderController.class.getName());
 
     private CalenderService calenderService;
+    private PdfGeneratorService pdfGeneratorService;
 
-    public CalenderController(CalenderService calenderService) {
+    public CalenderController(CalenderService calenderService, PdfGeneratorService pdfGeneratorService) {
         this.calenderService = calenderService;
+        this.pdfGeneratorService = pdfGeneratorService;
     }
 
     // C - create
@@ -38,6 +45,19 @@ public class CalenderController {
         modelMap.addAttribute("calculate", calculate);
         LOGGER.info("create(...)");
         return "calendar.html";
+    }
+
+    @GetMapping("/pdf/generate")
+    public void generatePDF(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("dd-mm-yyyy:hh:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=pdf_" + currentDateTime + "+pdf";
+        response.setHeader(headerKey,headerValue);
+
+        this.pdfGeneratorService.export(response);
     }
 }
 // TODO: 18.01.2023
